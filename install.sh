c0ac15c146f30ab4c98f0838d771062da25b45cb
cmaspi <chiragmehta1010@gmail.com>
2022-01-18 23:06:05 +0530

    uninstall.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ sudo apt-get -y install python3-pip &> /dev/null
 pip install pillow &> /dev/null
 set -x dir (pwd)
 set -x user (whoami)
-mv $dir /home/$user/.tasks/
+mv $dir /home/$user/.tasks/ 
 
 echo "alias tasks='fish /home/$user/.tasks/run.sh'" >> ~/.config/fish/config.fish
