8d0b568eec3477839a1510cb3b73e607ae8b4d15
cmaspi <chiragmehta1010@gmail.com>
2022-02-05 00:00:48 +0530

    hyperlinks added in tasks

--- run.sh
+++ run.sh
@@ -3,18 +3,28 @@
 set -x cmd $argv[1]
 
 switch $cmd
+    case ''
+        echo "tasks ... what? Refer man page for list of valid commands"
     case add
+        set -e _flag_link
+        argparse 'l/link=+' -- $argv   
         set -x lines (echo -n $argv[2..-1]| md5sum | cut -d " " -f 1| cut -b -7)
-        echo $lines $argv[2..-1] >> ~/.tasks/data/Tdo.csv
+        echo -n $lines $argv[2..-1]  >> ~/.tasks/data/Tdo.csv
+        if set -q _flag_link
+            for i in $_flag_link
+                set -x link (echo -e "\e]8;;$i\aResource\e]8;;\a")
+                echo -n , $link >> ~/.tasks/data/Tdo.csv
+            end
+        end
+        echo  >> ~/.tasks/data/Tdo.csv
 
     case delete
-        argparse 'n/index=?' -- $argv
-        set -S _flag_index
+        set -e _flag_index _flag_hash
+        argparse 'n/index=?' 'H/hash=?' -- $argv
         if set -q _flag_index
-        echo $_flag_index
-        end
-        sed -i "$_flag_index d" ~/.tasks/data/Tdo.csv
-    
+            sed -i "$_flag_index d" ~/.tasks/data/Tdo.csv
+        
+        end    
     case done
         argparse 'n/index'= -- $argv
         sed  "$_flag_index !d" ~/.tasks/data/Tdo.csv >> ~/.tasks/data/Tdone.csv
