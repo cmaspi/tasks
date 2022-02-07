c30deb968ed0c77a1f7d6d408972a8df0a16192d
cmaspi <chiragmehta1010@gmail.com>
2022-02-07 10:17:50 +0530

    fixed install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ mv $dir ~/.tasks/
 chmod 700 ~/.tasks/run.sh
 mkdir ~/.tasks/lists
 mkdir ~/.tasks/lists/base
-touch ~/.tasks/base/Tdo.csv
-touch ~/.tasks/base/Tdone.csv
+touch ~/.tasks/lists/base/Tdo.csv
+touch ~/.tasks/lists/base/Tdone.csv
 echo "set list base" > ~/.tasks/meta/env
 
 echo "alias tasks='fish ~/.tasks/run.sh'" >> ~/.config/fish/config.fish
