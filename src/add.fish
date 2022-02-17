0b7460c39f34f3673b11f86eb7573ce622727d25
cmaspi <chiragmehta1010@gmail.com>
2022-02-17 13:51:33 +0530

    Changed from .tasks to ./local/lib/tasks

--- src/add.fish
+++ src/add.fish
@@ -1,12 +1,12 @@
 function add
     argparse 'l/link=+' 'L/list=' -- $argv   
         set -x lines (echo -n $argv[2..-1]| md5sum | cut -d " " -f 1| cut -b -7)
-        echo -n "$lines $argv[2..-1] |" >> ~/.tasks/lists/$_flag_list/Tdo.csv
+        echo -n "$lines $argv[2..-1] |" >> $HOME/.local/lib/tasks/lists/$_flag_list/Tdo.csv
         if set -q _flag_link
             for i in $_flag_link
                 set -x link (echo -e "\e]8;;$i\aResource\e]8;;\a")
-                echo -n , $link >> ~/.tasks/lists/$_flag_list/Tdo.csv
+                echo -n , $link >> $HOME/.local/lib/tasks/lists/$_flag_list/Tdo.csv
             end
         end
-        echo  >> ~/.tasks/lists/$_flag_list/Tdo.csv
+        echo  >> $HOME/.local/lib/tasks/lists/$_flag_list/Tdo.csv
 end
