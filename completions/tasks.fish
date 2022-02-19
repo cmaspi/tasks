8354dcfec1c0160637f5635805c2f470a6d576f3
cmaspi <chiragmehta1010@gmail.com>
2022-02-19 23:31:07 +0530

    Added a contribute.txt file
    This file has been added to guide the new comers on this project.

--- completions/tasks.fish
+++ completions/tasks.fish
@@ -1,6 +1,5 @@
 # list of all available commands
-set -l commands 'help' create tear list use add addAt rm delete done clean clear cleanDo clearDo cleanDone clearDone display show displayDone showDone
-
+set -l commands 'help' mklist rmlist ls cl add addAt rm show vi xdg
 # command doesn't require files
 complete -c tasks -f
 
@@ -12,42 +11,27 @@ complete -c tasks -n "not __fish_seen_subcommand_from $commands" \
 complete -c tasks -n "not __fish_seen_subcommand_from $commands" \
     -a "rm" -d "deletes a task from to-do sublist"
 complete -c tasks -n "not __fish_seen_subcommand_from $commands" \
-    -a "show" -d "shows the do-do sublist"
+    -a "show" -d "prints both the sublists"
 complete -c tasks -n "not __fish_seen_subcommand_from $commands" \
-    -a "showDone" -d "shows the done sublist"
+    -a "mklist" -d "create another list"
 complete -c tasks -n "not __fish_seen_subcommand_from $commands" \
-    -a "create" -d "create another list"
+    -a "rmlist" -d "delete a list"
 complete -c tasks -n "not __fish_seen_subcommand_from $commands" \
-    -a "tear" -d "delete a list"
+    -a "ls" -d "prints the name of available lists"
 complete -c tasks -n "not __fish_seen_subcommand_from $commands" \
-    -a "list" -d "prints the name of available lists"
-complete -c tasks -n "not __fish_seen_subcommand_from $commands" \
-    -a "use" -d "use a particular list"
+    -a "cl" -d "use a particular list"
 complete -c tasks -n "not __fish_seen_subcommand_from $commands" \
     -a "addAt" -d "adds task at given index"
 complete -c tasks -n "not __fish_seen_subcommand_from $commands" \
-    -a "delete" -d "alias for rm"
+    -a "vi" -d "opens the to-do list in vi"
 complete -c tasks -n "not __fish_seen_subcommand_from $commands" \
-    -a "clean" -d "clears both to-do and done sublists"
-complete -c tasks -n "not __fish_seen_subcommand_from $commands" \
-    -a "clear" -d "alias for clean"
-complete -c tasks -n "not __fish_seen_subcommand_from $commands" \
-    -a "cleanDo" -d "clears to-do sublist"
-complete -c tasks -n "not __fish_seen_subcommand_from $commands" \
-    -a "clearDo" -d "alias for cleanDo"
-complete -c tasks -n "not __fish_seen_subcommand_from $commands" \
-    -a "cleanDone" -d "clears the done sublist"
-complete -c tasks -n "not __fish_seen_subcommand_from $commands" \
-    -a "clearDone" -d "alias for cleanDone"
-complete -c tasks -n "not __fish_seen_subcommand_from $commands" \
-    -a "display" -d "alias for show"
-complete -c tasks -n "not __fish_seen_subcommand_from $commands" \
-    -a "displayDone" -d "alias for showDone"
+    -a "xdg" -d "opens the to-do list in default text editor"
+
 
 # auto-complete for tasks use
-complete -c tasks -n "__fish_seen_subcommand_from use"\
+complete -c tasks -n "__fish_seen_subcommand_from cl"\
     -a "(ls $HOME/.local/lib/tasks/lists)"
 
 # auto-complete for tasks tear
-complete -c tasks -n "__fish_seen_subcommand_from tear"\
+complete -c tasks -n "__fish_seen_subcommand_from rmlist"\
     -a "(ls $HOME/.local/lib/tasks/lists)"
